0648e51bcc549b8a230083acc0b8228c46249e9e
stonedseeker <vaibhav24k@gmail.com>
2024-06-05 14:35:57 +0530

    Remove Duplicated From Sorted Array

--- RemoveDuplicatesSortedList.py
+++ RemoveDuplicatesSortedList.py
@@ -9,17 +9,24 @@ class ListNode:
 
 class Solution:
     def duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if head == None or head.next == None:
+            return head
+
         curr = head
-        while curr.next :
+        while curr.next:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
-            
-            curr = curr.next
+    
+            else:
+                curr = curr.next
 
+       
         return head
 
 
-head = ListNode(1, ListNode(1, ListNode(2, None)))
+head = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3, None)))))
+head = ListNode(1, ListNode(1, ListNode(1, None)))
 
 curr = Solution().duplicates(head)
 
