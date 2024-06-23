5bf940452706c811718ca4493f71ec088c871d4f
stonedseeker <vaibhav24k@gmail.com>
2024-06-24 01:01:02 +0530

    Inorder and Postorder BST Creation

---
+++ BSTFromInorderPostorder.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+
+
+import builtins
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+      
+        if not preorder:
+            return None 
+        
+
+        root, index = TreeNode(preorder[0]), 0
+        for i in range(len(inorder)):
+            if inorder[i] == root.val:
+                index = i
+
+        root.left = self.buildTree(preorder[1 : index + 1], inorder[:index])
+        root.right = self.buildTree(preorder[index+1 :], inorder[index + 1 : ])
+
+
+        
