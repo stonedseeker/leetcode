d7d9f434cb0533b3be5b18175e9d1c994928fafb
= <=>
2025-07-19 17:16:10 +0530

    valid sudoku

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -21,7 +21,33 @@ class Solution:
                 squares[(r//3, c//3)].add(grid[r][c])
         return True
 
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        grid = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.': continue
+                if board[r][c] in rows[r]: 
+                    return False
+                else: 
+                    rows[r].add(board[r][c])
+
+                if board[r][c] in cols[c]: 
+                    return False
+                else: 
+                    cols[c].add(board[r][c])
+
+                if board[r][c] in grid[(r//3,c//3)]: 
+                    return False
+                else: 
+                    grid[(r//3,c//3)].add(board[r][c])
+        
+        return True
 
       #     0   1   2   3   4   5   6   7   8
 board = [[".",".",".",".","5",".",".","1","."],
