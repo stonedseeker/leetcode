c9d1879da87d6b1ce671f5d8832fbe178ac6ef33
stonedseeker <vaibhav24k@gmail.com>
2025-07-19 19:34:17 +0530

    'lcs;

--- lcs.py
+++ lcs.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_prev:
     def lcs(self,s,t):
         m = len(s)
         n = len(t)
@@ -12,7 +12,29 @@ class Solution:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
         return dp[m][n]
 
+
+
+
+
+class Solution:
+    def lcs(self, s, t):
+        m = len(s)
+        n = len(t)
+
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[m][n]
+
+
 print(Solution().lcs("heap", "pea"))
+
 
 
 ''''
